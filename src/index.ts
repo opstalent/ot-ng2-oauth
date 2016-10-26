3a2445c5fd96086c258493ff595d823c290d0fae
Mateusz Bosek <bosek.mateusz@gmail.com>
2016-10-26 14:14:56 +0200

    Change bareling paths

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './directives/IsLoggedIn.directive';
-export * from './directives/role.directive';
-
-export * from './services/auth.service';
-export * from './services/oauth.service';
-export * from './services/http.service';
-
-export * from './oauth.module';
